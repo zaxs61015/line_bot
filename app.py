94ee8a10df78fe3dcfd3b632a8829c5b98ca8bb8
zaxs61015 <76506674+zaxs61015@users.noreply.github.com>
2022-01-07 13:19:00 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -101,6 +101,8 @@ def drawStraws():
     return drawStrawsList[random.randint(0, len(drawStrawsList) - 1)]
 
 
-keep_alive()
+# if __name__ == "__main__":
+  #  app.run()
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
